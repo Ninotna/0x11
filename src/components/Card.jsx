6f8c063a642fa555929cf8369dcdf957a71ff478
ninotna <antonin@dev87.fr>
2025-01-31 16:07:10 +0100

    Code Review with the mentor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,9 @@
 import { Link } from "react-router-dom";
+// Link est un composant de navigation qui permet de naviguer entre les différentes pages de l'application.
+// React-router-dom est une librairie qui permet de gérer la navigation dans une application React.
+// Cela change le contenu à l'intérieur de la balise <div> sans recharger la page.
+// On est sur une one single page application (SPA) qui ne se recharge pas à chaque changement de page.
+// On a l'url qui change mais pas la page.
 import "../assets/styles/card.scss";
 
 const Card = ({ id, title, cover }) => {
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,31 @@
+import Banner from "../components/Banner";
+import Collapse from "../components/Collapse";
+import "../assets/styles/about.scss";
+
 const About = () => {
-    return <h1>À propos de nous</h1>;
+    return (
+        <div className="about">
+            <Banner image="/assets/about-banner.jpg" text="En savoir plus sur Kasa" />
+            <div className="about-content">
+                <Collapse 
+                    title="Fiabilité" 
+                    content="Chez Kasa, nous garantissons que toutes les annonces sont vérifiées pour assurer une expérience de confiance totale." 
+                />
+                <Collapse 
+                    title="Respect" 
+                    content="Nous nous engageons à promouvoir des comportements respectueux entre les utilisateurs, et à maintenir un environnement harmonieux." 
+                />
+                <Collapse 
+                    title="Service" 
+                    content="Notre service client est disponible pour répondre à vos besoins et garantir une expérience fluide à tout moment." 
+                />
+                <Collapse 
+                    title="Sécurité" 
+                    content="La sécurité est une priorité. Nous mettons en œuvre des standards stricts pour protéger nos hôtes et leurs invités." 
+                />
+            </div>
+        </div>
+    );
 };
 
 export default About;
