30208e70175036b8eafff4482b8984f6d6eedb08
ninotna <antonin@dev87.fr>
2025-02-05 00:08:16 +0100

    feat: Ajout de la page NotFound feat: Redirection des routes inexistantes vers NotFound style: Ajout des styles pour la page d'erreur 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+import "../assets/styles/notfound.scss";
+
 const NotFound = () => {
-    return <h1>404 - Page non trouvée</h1>;
+    return (
+        <div className="notfound">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/">Retourner à la page d'accueil</Link>
+        </div>
+    );
 };
 
 export default NotFound;
+
