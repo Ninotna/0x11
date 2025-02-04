a10d4970cbbaee387ea85d6978d819f084b39cf3
ninotna <antonin@dev87.fr>
2025-02-05 00:24:57 +0100

    feat: Ajout des informations du logement sur la page PropertyDetails feat: Intégration du composant Collapse dans la page PropertyDetails style: Ajout des styles pour la page PropertyDetails

--- src/pages/PropertyDetails.jsx
+++ src/pages/PropertyDetails.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
-import logementsData from "../data/logements.json"; // Fichier contenant les données des propriétés
+import logementsData from "../data/logements.json";
 import Slideshow from "../components/Slideshow";
+import Collapse from "../components/Collapse";
+import "../assets/styles/propertydetails.scss";
 
 const PropertyDetails = () => {
     const { id } = useParams();
@@ -10,13 +12,47 @@ const PropertyDetails = () => {
         return <h1>Propriété introuvable</h1>;
     }
 
-        // Mapper "pictures" en "images"
-        const images = property.pictures;
-
     return (
         <div className="property-details">
-            <h1>{property.title}</h1>
-            <Slideshow images={images} />
+            <Slideshow images={property.pictures} />
+            <div className="property-header">
+                <div>
+                    <h1>{property.title}</h1>
+                    <p>{property.location}</p>
+                    <div className="tags">
+                        {property.tags.map((tag, index) => (
+                            <span key={index} className="tag">{tag}</span>
+                        ))}
+                    </div>
+                </div>
+                <div className="host">
+                    <p>{property.host.name}</p>
+                    <img src={property.host.picture} alt={property.host.name} />
+                </div>
+            </div>
+            <div className="property-rating">
+                {[...Array(5)].map((_, index) => (
+                    <span
+                        key={index}
+                        className={`star ${index < property.rating ? "filled" : ""}`}
+                    >
+                        ★
+                    </span>
+                ))}
+            </div>
+            <div className="property-collapses">
+                <Collapse title="Description" content={property.description} />
+                <Collapse
+                    title="Équipements"
+                    content={
+                        <ul>
+                            {property.equipments.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul>
+                    }
+                />
+            </div>
         </div>
     );
 };
