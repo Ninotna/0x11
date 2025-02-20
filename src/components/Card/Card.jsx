2e069ead16dcdfcf71ec99c74c673057695ae84a
ninotna <antonin@dev87.fr>
2025-02-20 14:34:47 +0100

    Component Card: Implémente le composant Card avec NavLink pour la navigation dynamique et ajoute les styles SCSS associés

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,25 @@
-import { Link } from "react-router-dom";
-// Link est un composant de navigation qui permet de naviguer entre les différentes pages de l'application.
-// React-router-dom est une librairie qui permet de gérer la navigation dans une application React.
-// Cela change le contenu à l'intérieur de la balise <div> sans recharger la page.
-// On est sur une one single page application (SPA) qui ne se recharge pas à chaque changement de page.
-// On a l'url qui change mais pas la page.
+import { NavLink } from "react-router-dom";
+// utilisation de NavLink pour naviguer entre les pages plutot que
+// Link pour avoir un style différent
 
-const Card = ({ data }) => {
+function Card({ data }) {
   return (
-    <Link to={`/housing/${data.id}`} className="card">
-      <img src={data.cover} alt={data.title} />
-      <div className="overlay">
-        <h2>{data.title}</h2>
-      </div>
-    </Link>
+    <article className="card__accomodation">
+      <NavLink
+        className="card__acccomodation--link"
+        to={`/logement/${data.id}`}
+        state={{ accomodationDatas: data }}
+        // state={{ test404: data }}
+      >
+        <div className="card__accomodation--img">
+          <img src={data.cover} alt={`Lien vers l'hébergement ${data.title}`} />
+        </div>
+        <div className="card__accomodation--caption">
+          <h3>{data.title}</h3>
+        </div>
+      </NavLink>
+    </article>
   );
-};
+}
 
 export default Card;
