76326760a0496b06ada94bd69ed85be76de226a7
ninotna <antonin@dev87.fr>
2025-02-20 15:28:41 +0100

    MAJ de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
 // import PropertyDetails from "./pages/Property/PropertyDetails";
-// import About from "./pages/About/About";
+import About from "./pages/About/About";
 import Error404 from "./pages/NotFound/Error404";
 
 function App() {
@@ -11,8 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/property/:id" element={<PropertyDetails />} /> */}
-        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/logement/:id" element={<PropertyDetails />} /> */}
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
